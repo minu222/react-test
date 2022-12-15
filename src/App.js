ca4b6dc46f45febca9e1c7969d93d0be63493aba
박기수 <gisu130@gmail.com>
2022-12-15 19:32:18 +0900

    임시 코드

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import StopWatch from "./StopWatch";
+import NumberCounter from "./NumberCounter";
+
+function App() {
+  return (
+    <>
+      {/* <StopWatch /> */}
+      <NumberCounter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/NumberCounter.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function NumberCounter() {
+  const [no, setNo] = useState(0);
+
+  const noIsEvenDiv = (
+    <>
+      <hr /> {no % 2 == 0 ? <div>짝수입니다.</div> : <div>홀수입니다.</div>}
+    </>
+  );
+
+  const noIs8MultipleDiv = no % 8 == 0 || (
+    <>
+      <hr />
+      <div>8의 배수가 아닙니다.</div>
+    </>
+  );
+
+  return (
+    <>
+      숫자 : {no}
+      <hr />
+      <button onClick={() => setNo(no + 1)}>증가</button>
+      {noIsEvenDiv}
+      {noIs8MultipleDiv}
+    </>
+  );
+}
+
+export default NumberCounter;
